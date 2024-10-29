efd57fe55ebee7194e05a26696a761570ea645ad
Nicholas Gallina <nicholasgallina@Nicholas-Gallinas-MacBook-Air.local>
2024-10-29 09:22:52 -0400

    added some button functionality

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+document.getElementById('aboutMeButton').onclick = function () {
+
+  window.location.href = about.html;
+
+};
+
+document.getElementById('resumeButton').onclick = function () {
+
+  window.location.href = resume.html;
+
+};
+
+
+document.getElementById('projectsButton').onclick = function () {
+
+  window.location.href = projects.html;
+
+};
